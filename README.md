3698eccd557f65e5007dadc3127ef8ef107e4e62
Shubham Kanodia <shubham.kanodia10@gmail.com>
2020-04-06 19:38:14 +0530

    Adds US State level data

--- README.md
+++ README.md
@@ -1,38 +1,55 @@
 # Covid-19 Mobility Tracker
-[Google Mobility Reports](https://www.google.com/covid19/mobility/) show aggregate activity in each country, 
+
+[Google Mobility Reports](https://www.google.com/covid19/mobility/) show aggregate activity in each country,
 and how it changes in response to policies aimed at combating COVID-19. However, it is only published as a PDF
 and the data isn't available in a machine-readable format that could enable more richer analysis.
 
 **This is an effort to reverse-engineer the PDFs into vectors and ultimately into time-series data available as a JSON Rest API.**
 
-![alt text](https://github.com/pastelsky/covid-19-mobility-tracker/raw/master/code.png "")
+![alt text](https://github.com/pastelsky/covid-19-mobility-tracker/raw/master/code.png)
 
 ## Usage
+
+### Country-level
+
 The JSON is available for use at –
+
 ```
 https://pastelsky.github.io/covid-19-mobility-tracker/output/<ISO-COUNTRY-CODE>/mobility.json
 ```
 
-Each data element consists of `date` and `value` (which represents mobility change in percentage)
-
 For eg: India: https://pastelsky.github.io/covid-19-mobility-tracker/output/IN/mobility.json
 
+## US State-level
+
+The JSON is available at –
+
+```
+https://pastelsky.github.io/covid-19-mobility-tracker/output/US/<US-STATE-CODE>/mobility.json
+```
+
+For eg: New York: https://pastelsky.github.io/covid-19-mobility-tracker/output/US/NY/mobility.json
+
+Each data element consists of `date` and `value` (which represents mobility change in percentage)
+
 ## Limitations
-- Expected error range is ±2%, though errors in activity percentanges 
- should be rare given the methodolgy used to extract data.
-- As of now, this only indexes country-level data. State / Province level breakup is yet to be added.
+
+- Expected error range is ±2%, though errors in activity percentanges
+  should be rare given the methodolgy used to extract data.
+- As of now, this only indexes country-level data and US state level data. State / Province level breakup for other countries is yet to be added.
 - Data sources will be updated once / day and is incumbent upon google publishing updated reports
 - It assumes that the same scale (-80% to +80%) is used in all PDFs and they are structured similarly.
 
 ## Contributing
+
 1. Run `yarn install` to install dependencies.
-2. Install Inkscape `1.0beta2` for your OS, and make sure the `inkscape` is available 
-as in your path as a command line utility.
-You can test this using `inkscape --version`
+2. Install Inkscape `1.0beta2` for your OS, and make sure the `inkscape` is available
+   as in your path as a command line utility.
+   You can test this using `inkscape --version`
 
 3. Run `yarn build` to begin downloading reports form google, and parsing it into the `ouput` folder.
 
 ## Credits
-All data made available for use is by taken from Google Mobility Reports. 
-This project does not claim any ownership over this data.
 
+All data made available for use is by taken from Google Mobility Reports.
+This project does not claim any ownership over this data.
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const countryCodes = require('country-codes-list');
 const rimraf = require('rimraf');
 const { default: PQueue } = require('p-queue');
 const { default: Worker } = require('jest-worker');
+const USStates = require('./lib/us-states');
 
 const DATA_LAST_AVAILABLE_FOR_DATE = '2020-03-29';
 const DOWNLOAD_CONCURRENCY = 20;
@@ -25,6 +26,16 @@ const chartTypes = [
 ];
 
 const countryCodesList = Object.keys(countryCodes.customList('countryCode', '[{countryCode}]'));
+const USStateCodeList = Object.values(USStates).map(
+  (stateName) => `US_${stateName.replace(/ /g, '_')}`
+);
+const getUSStateCodeFromStateNameCode = (stateNameCode) => {
+  const matchedState = Object.entries(USStates).find(
+    ([stateCode, stateName]) => stateName === stateNameCode.replace('US_', '').replace(/_/g, ' ')
+  );
+  return matchedState[0];
+};
+
 const downloadQueue = new PQueue({ concurrency: DOWNLOAD_CONCURRENCY });
 const processQueue = new PQueue({ concurrency: PROCESS_CONCURRENCY });
 const pdfsPath = path.join(__dirname, 'pdfs');
@@ -42,39 +53,44 @@ function cleanPath(pathToClean) {
   rimraf.sync(pathToClean);
 }
 
-function downloadForAllCountries() {
-  const successCountryCodes = [];
-  cleanPath(pdfsPath);
+function downloadForAll(geographies) {
+  const successGeographyCodes = [];
 
-  countryCodesList.forEach(async (countryCode) => {
+  geographies.forEach(async (geographyCode) => {
     try {
-      await downloadQueue.add(() => downloadPdf(countryCode));
-      successCountryCodes.push(countryCode);
+      await downloadQueue.add(() => downloadPdf(geographyCode));
+      successGeographyCodes.push(geographyCode);
     } catch (err) {
-      console.log('Failed to download for ', countryCode);
+      console.log('Failed to download for ', geographyCode);
     }
   });
+
   console.log(
     'Total: ',
-    countryCodesList.length,
+    geographies.length,
     'Download Succeeded: ',
-    successCountryCodes.length,
+    successGeographyCodes.length,
     'Download Failed: ',
-    countryCodesList.length - successCountryCodes.length
+    geographies.length - successGeographyCodes.length
   );
-  return downloadQueue.onIdle().then(() => successCountryCodes);
+  return downloadQueue.onIdle().then(() => successGeographyCodes);
 }
 
-function processForAllCountries(countryCodes) {
+function processForAll(geographyCodes, stateFromCountryCode) {
   const processWorker = new Worker(require.resolve('./parseFromPdf'));
 
-  cleanPath(outputPath);
+  geographyCodes.forEach(async (geographyCode) => {
+    const fileOutputPath = stateFromCountryCode
+      ? path.join(
+          __dirname,
+          'output',
+          stateFromCountryCode,
+          getUSStateCodeFromStateNameCode(geographyCode)
+        )
+      : path.join(__dirname, 'output', geographyCode);
 
-  countryCodes.forEach(async (countryCode) => {
-    const fileOutputPath = path.join(__dirname, 'output', countryCode);
-
-    console.log('Processing...', countryCode);
-    const pdfPath = path.join(pdfsPath, countryCode, 'mobility.pdf');
+    console.log('Processing...', geographyCode);
+    const pdfPath = path.join(pdfsPath, geographyCode, 'mobility.pdf');
     const page1Charts = await processQueue.add(() =>
       processWorker.processPDFPage(pdfPath, 1, fileOutputPath)
     );
@@ -86,9 +102,9 @@ function processForAllCountries(countryCodes) {
     if (charts.length !== 6) {
       throw new Error('Expected to receive 6 graphs, got: ', charts.length);
     }
-    const json = { country: {} };
+    const json = stateFromCountryCode ? { state: {} } : { country: {} };
     charts.forEach((chart, i) => {
-      json.country[chartTypes[i]] = chart;
+      json[stateFromCountryCode ? 'state' : 'country'][chartTypes[i]] = chart;
     });
 
     fs.writeFileSync(
@@ -97,7 +113,7 @@ function processForAllCountries(countryCodes) {
       'utf8'
     );
 
-    console.log('Wrote to file...', countryCode);
+    console.log('Wrote to file...', geographyCode);
   });
 
   return processQueue.onIdle();
@@ -105,8 +121,13 @@ function processForAllCountries(countryCodes) {
 
 async function run() {
   try {
-    const countryCodes = await downloadForAllCountries();
-    await processForAllCountries(countryCodes);
+    cleanPath(pdfsPath);
+    const countryCodes = await downloadForAll(countryCodesList);
+    const USStateCodes = await downloadForAll(USStateCodeList);
+
+    cleanPath(outputPath);
+    await processForAll(countryCodes);
+    await processForAll(USStateCodes, 'US');
   } catch (err) {
     console.error(err);
   }
